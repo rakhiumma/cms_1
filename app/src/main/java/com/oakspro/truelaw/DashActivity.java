ecf18834faaf8969bf13c52a47c2d2c6cae37c95
rakhi15 <mail2k.rakhi@gmail.com>
2022-02-02 11:20:51 +0530

    till 02-02-2022

---
+++ app/src/main/java/com/oakspro/truelaw/DashActivity.java
@@ -0,0 +1,79 @@
+package com.oakspro.truelaw;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+import com.oakspro.truelaw.databinding.ActivityDashBinding;
+
+public class DashActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private ActivityDashBinding binding;
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityDashBinding.inflate(getLayoutInflater());
+        setTitle(getResources().getString(R.string.app_name));
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+
+        drawerLayout=findViewById(R.id.drawerLayout);
+        navigationView=findViewById(R.id.navigation_view);
+
+        toggle=new ActionBarDrawerToggle(this, drawerLayout, R.string.start, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView.setNavigationItemSelectedListener(this);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_dash);
+       // NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.navigation_discussion:
+                Toast.makeText(this, "Discussion", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navigation_more:
+                Toast.makeText(this, "More", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return true;
+    }
+}
--- app/src/main/java/com/oakspro/truelaw/SignupActivity.java
+++ app/src/main/java/com/oakspro/truelaw/SignupActivity.java
@@ -2,6 +2,7 @@ package com.oakspro.truelaw;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -68,6 +69,9 @@ public class SignupActivity extends AppCompatActivity {
                     backTxt.setVisibility(View.GONE);
                     nextBtn.setVisibility(View.GONE);
                     loadingBar.setVisibility(View.VISIBLE);
+
+                    Intent intent=new Intent(SignupActivity.this, DashActivity.class);
+                    startActivity(intent);
 
                 }
 
